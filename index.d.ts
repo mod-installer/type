e0212570af1023351c8dcad8d660f470ed06712a
Björn Büttner <Idrinth@users.noreply.github.com>
2024-11-05 13:55:38 +0100

    Create index.d.ts
    just preparation

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+interface MI1_0_0 = {}
+interface MI1_0_1 extends MI1_0_0 {}
+interface MI1_0_2 extends MI1_0_1 {}
+interface MI1_1_0 extends MI1_0_2 {}
+
+export MI1_0_0;
+export MI1_0_1;
+export MI1_0_2;
+export MI1_1_0;
+
+export default MI1_1_0;//latest
