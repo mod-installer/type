74226e494df21c89c293b529b555836b389718cb
Björn Büttner <Idrinth@users.noreply.github.com>
2024-11-06 05:13:54 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,62 @@
-interface MI1_0_0 = {}
+interface MI1_0_0 = {
+  schemaVersion: string;
+  name: string;
+  version: string;
+  authors?: {
+    name: string;
+    roles?: string[];
+    description?: string;
+    image?: string;
+    website?: string;
+  }[];
+  websites?: string[];
+  image?: string;
+  readme?: string;
+  setFileSystemFlags?: {
+    [path: string]: string;
+  };
+  steps?: {
+    name: string;
+    groups: {
+      name: string;
+      mode: string;
+      failure?: string;
+      options?: {
+        [option: string]: {
+          requiredFlags?: string[];
+          description: string[];
+          image: string;
+          forbiddenFlags?: string[];
+          setFlags?: string[];
+        }
+      }
+    }[]:
+    readme?: string;
+    requiredFlags?: string[];
+    forbiddenFlags?: string[];
+  }[];
+  installFolders?: {
+    from: string;
+    to: string;
+    requiredFlags?: string[];
+    forbiddeFlags?: string[];
+  }[];
+  installFiles?: {
+    from: string;
+    to: string;
+    requiredFlags?: string[];
+    forbiddeFlags?: string[];
+  }[];
+}
 interface MI1_0_1 extends MI1_0_0 {}
 interface MI1_0_2 extends MI1_0_1 {}
 interface MI1_1_0 extends MI1_0_2 {}
+interface MI1_2_0 extends MI1_1_0 {}
 
 export MI1_0_0;
 export MI1_0_1;
 export MI1_0_2;
 export MI1_1_0;
+export MI1_2_0;
 
-export default MI1_1_0;//latest
+export default MI1_2_0;//latest
